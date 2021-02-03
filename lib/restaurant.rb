20370df6483f514d155e117d0bb586498833ce60
Kseniya Turing <kseniya.kudelich@gmail.com>
2021-02-03 14:54:48 -0700

    Add whatever I could

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -12,14 +12,21 @@ def closing_time(work_hours)
   "#{(@opening_time.to_i + work_hours)}" + ":00"
 end
 
-
-
 def add_dish(dishes)
   @dishes << dishes
 end
 
-# def open_for_lunch?
-#   @opening_time < 12
+def open_for_lunch?
+  if @opening_time.to_i < 12
+    true
+  else
+    false
+  end
+end
+
+# def menu_dish_names
+#   @dishes.map! do |dishes|
+#     dishes.upcase
 # end
 
 end
--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -53,12 +53,19 @@ class RestaurantTest < Minitest::Test
   end
 
 
-  # def test_if_open_for_lunch
-  #     restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
-  #     restaurant2 = Restaurant.new('16:00', 'Il Posto')
-  #     assert_equal true, @restaurant.open_for_lunch?
+  def test_if_open_for_lunch?
+      restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+      restaurant2 = Restaurant.new('16:00', 'Il Posto')
+
+      assert_equal true, @restaurant.open_for_lunch?
+  end
+  #
+  # def test_menu_dish_names
+  #   restaurant2.add_dish('Burrata')
+  #   restaurant2.add_dish('Pizzetta')
+  #   restaurant2.add_dish('Ravioli')
+  #   assert_equal ["BURRATA", "PIZZETTA", "RAVIOLI"], @restaurant.menu_dish_names
+  #
   # end
-
-
 
 end
